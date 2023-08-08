3acbd71d1b5960a6ec75dbc33f4557ffaffa7d4f
Roman <Cherkasov.R.V@sberbank.ru>
2023-08-09 00:35:45 +0300

    dynamic polimorphism

--- 12_read/main.cpp
+++ 12_read/main.cpp
@@ -23,13 +23,14 @@ public:
     std::string DescribeSorted() {
         bool first = true;
         std::stringstream ss;
+        ss.clear();
         ss << "store=[";
         for(User* ptr: sorted_){
             if(first) first=false;
-            else std::cout << ", ";
+            else ss << ", ";
             ss <<"<"<<ptr->user_id<<","<<ptr->page_num<<">";
         }
-        ss << "]"<<std::endl;
+        ss << "]";
         return ss.str();
     }
     std::string DescribeUser2Idx(){
@@ -38,33 +39,48 @@ public:
         ss << "user2idx={";
         for(const auto& [user_id, idx]:user2idx_) {
             if(first) first=false;
-            else std::cout << ", ";
+            else ss << ", ";
             ss <<"<"<<user_id<<":"<<idx<<">";
         }
+        ss << "}";
         return ss.str();
     }
 
+    index_t MoveUpdatedUser(index_t user_id){
+        int curr=user2idx_[user_id],
+            next=curr+1;
+        while (
+            next < sorted_.size()
+            &&
+            sorted_[curr]->page_num > sorted_[next]->page_num
+        )
+        {
+            
+            user2idx_[sorted_.at(next)->user_id] = curr;
+            user2idx_[sorted_.at(curr)->user_id] = next;
+            std::swap(sorted_[curr], sorted_[next]);
+            curr++;
+            next++;
+        }
+        return curr-1;
+    }
+    void CorrectAfterInsert(index_t user_id){
+        size_t idx = user2idx_[user_id];
+        for(size_t i=idx+1; i<sorted_.size();++i) {
+            user2idx_[sorted_.at(i)->user_id]++;
+        }
+    }
+
     void SetUser(const index_t user_id, const number_t page) {
-        if(is_debug){
+        if(is_debug) {
             std::cout << "[SetUser] before: item=<" << user_id << "," << page <<"> ";
-            std::cout << DescribeSorted();
+            std::cout << DescribeSorted() << " ";
             std::cout << DescribeUser2Idx() << std::endl;
         }
         if(user2idx_.count(user_id)) {
             size_t idx = user2idx_.at(user_id);
             sorted_[idx]->page_num = page;
-            idx++;
-            while (idx<sorted_.size() && sorted_[idx]<sorted_[idx-1]) {
-                user2idx_[sorted_[idx]->user_id] = idx-1;
-                std::swap(sorted_[idx-1],sorted_[idx]);
-                idx++;
-
-            }
-            while (idx<sorted_.size()){
-                user2idx_[sorted_[idx]->user_id] = idx+1;
-                idx++;
-            }
-            user2idx_[user_id] = idx;
+            
         }
         else {
             User* user_ptr = new User{user_id, page};
@@ -79,8 +95,29 @@ public:
             auto res = sorted_.insert(it, user_ptr);
             user2idx_[user_id] = std::distance(sorted_.begin(), res);
         }
+        idx++;
+        while (idx<sorted_.size() && sorted_[idx]<sorted_[idx-1]) {
+            std::cout 
+                << "[u2i] "
+                << sorted_[idx]->user_id << ": "
+                << idx << " -> " << idx-1 << std::endl;
+            user2idx_[sorted_[idx]->user_id] = idx-1;
+            std::swap(sorted_[idx-1],sorted_[idx]);
+            idx++;
+
+        }
+        while (idx<sorted_.size()){
+            std::cout 
+                << "[u2i] "
+                << sorted_[idx]->user_id << ": "
+                << idx << " -> " << idx+1 << std::endl;
+            user2idx_[sorted_[idx]->user_id] = idx+1;
+            idx++;
+        }
+        user2idx_[user_id] = idx;
         if(is_debug){
-            std::cout << "[SetUser] after:" << DescribeSorted() << DescribeUser2Idx() << std::endl;
+            std::cout << "[SetUser] after:" << DescribeSorted() 
+            <<" " << DescribeUser2Idx() << std::endl;
         }
     }
 
